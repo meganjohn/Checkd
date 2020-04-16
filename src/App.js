853309cb5be25259a78dc9f95e8d9f8e42ba7db2
lawcia <lawrencianjume@gmail.com>
2020-04-16 23:52:07 +0100

    create frontend routes for submitnews and admin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import ReviewForm from './components/ReviewForm/ReviewForm';
-import Auth from './components/Auth/Auth';
-import './App.css';
+import React from "react";
+import ReviewForm from "./components/ReviewForm/ReviewForm";
+import Auth from "./components/Auth/Auth";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      <ReviewForm />
-      <Auth />
-    </React.Fragment>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/"> Home </Link>
+            </li>
+            <li>
+              <Link to="/submit-news"> Submit News </Link>
+            </li>
+            <li>
+              <Link to="/admin-login"> Admin Login </Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" render={() => <h1> Welcome to Checkd </h1>} />
+          <Route path="/submit-news" component={ReviewForm} />
+          <Route path="/admin-login" component={Auth} />
+          <Route
+            path="/admin-dashboard"
+            render={() => <h1> Admin Dashboard </h1>}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
