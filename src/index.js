2848fe58eac88cf36441a081b56cbba898f7c168
lawcia <lawrencianjume@gmail.com>
2020-04-16 20:14:30 +0100

    firebase frontend config

--- src/index.js
+++ src/index.js
@@ -3,11 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const {
+  REACT_APP_FIRE_API_KEY,
+  REACT_APP_FIRE_PROJECT_ID,
+  REACT_APP_FIRE_MSG_SENDER_ID,
+  REACT_APP_FIRE_APP_ID
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_FIRE_API_KEY,
+  authDomain: `${REACT_APP_FIRE_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${REACT_APP_FIRE_PROJECT_ID}.firebaseio.com`,
+  projectId: REACT_APP_FIRE_PROJECT_ID,
+  storageBucket: `${REACT_APP_FIRE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: REACT_APP_FIRE_MSG_SENDER_ID,
+  appId: REACT_APP_FIRE_APP_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render( <
+  React.StrictMode >
+  <
+  App / >
+  <
+  /React.StrictMode>,
   document.getElementById('root')
 );
 
