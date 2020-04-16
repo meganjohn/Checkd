11b96f2e651fa709abdb0b87adfd23026bb05b72
lawcia <lawrencianjume@gmail.com>
2020-04-16 21:08:05 +0100

    create Auth.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReviewForm from './components/ReviewForm/ReviewForm';
+import Auth from './components/Auth/Auth';
 import './App.css';
 
 function App() {
   return (
+    <React.Fragment>
       <ReviewForm />
+      <Auth />
+    </React.Fragment>
   );
 }
 
