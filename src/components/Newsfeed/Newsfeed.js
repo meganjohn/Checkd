ea459165d22b3a3dbc581f547acdf5bfc4b10b3d
alystroud <alygrace@hotmail.co.uk>
2020-04-18 12:08:40 +0100

    Adding new fields to newsfeed items

--- src/components/Newsfeed/Newsfeed.js
+++ src/components/Newsfeed/Newsfeed.js
@@ -47,7 +47,9 @@ class Newsfeed extends React.Component<{}> {
         <div><a href={article.url}>{article.title}</a></div>
         <div>Sentiment: {article.sentiment}</div>
         <div>Polarity: {article.degree + " " + article.direction}</div>
+        <div>Objectivity: {article.objectivity}</div>
         <div>Verified: {article.verified ? "Yes" : "No"}</div>
+        <div>Source: </div>
       </div>
     );
   }
