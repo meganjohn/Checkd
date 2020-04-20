9a40c7a4b236cd7652cc2174c5dc29d46d4e969f
lawcia <lawrencianjume@gmail.com>
2020-04-21 00:32:06 +0100

    create loading overlay

--- src/components/SubmitNews/SubmitNews.js
+++ src/components/SubmitNews/SubmitNews.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReviewForm from "../ReviewForm/ReviewForm";
+import { Loading } from "carbon-components-react";
 import "./SubmitNews.css";
 class SubmitNews extends React.Component {
+  state = {
+    loading: false
+  }
+
+  setLoading = (value) => {
+    this.setState({loading: value})
+  }
+
   render() {
     return (
       <div className="SubmitNews">
+        {this.state.loading && (<div className="submitnews-loading"><Loading
+    description="Active loading indicator" withOverlay={false}
+  /></div>)}
         <div className="submitnews-card">
         <h1><b>Submit News</b></h1>
-        <ReviewForm />
+        <ReviewForm setLoading={this.setLoading}/>
         </div>
       </div>
     );
