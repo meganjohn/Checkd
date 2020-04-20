8b4f634d81acc8ce087b0342727fbdabfcf6d960
lawcia <lawrencianjume@gmail.com>
2020-04-20 23:36:16 +0100

    added styles for submitnews

--- src/components/SubmitNews/SubmitNews.js
+++ src/components/SubmitNews/SubmitNews.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReviewForm from "../ReviewForm/ReviewForm";
-import "./SubmitNews";
+import "./SubmitNews.css";
 class SubmitNews extends React.Component {
   render() {
     return (
       <div className="SubmitNews">
+        <div className="submitnews-card">
+        <h1><b>Submit News</b></h1>
         <ReviewForm />
+        </div>
       </div>
     );
   }
