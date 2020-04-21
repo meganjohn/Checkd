edad159564a2670b67648d66fe81cef6a98484ed
Megan John <meganjohn.791@gmail.com>
2020-04-21 21:16:54 +0100

    Setting up html foundations

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,10 +1,48 @@
 import React from "react";
+import Axios from "axios";
+import "./NavigationBar.css";
 
 function NavigationBar () {
     return (
-        <nav>
-            <h1>Checkd</h1>
-        </nav>
+        <div className="sticky-nav-bar">
+          <nav className="nav-bar">
+            <div className="left-group">
+              <div>
+                <Link to="/"> Checkd </Link>
+              </div>
+              <div className="line-spacer"></div>
+              <div>
+                <Link to="/newsfeed"> Sumbit News </Link>
+              </div>
+              <div>
+                <Link to="/submit-news"> News Feed </Link>
+              </div>
+              <div>
+                <Link to="/about-us">About us</Link>
+              </div>
+            </div>
+            <div className="right-group">
+              <div>
+                {/* logo */}
+              {this.state.loggedIn && (
+                <li>
+                  <Link to="/dashboard">Dashboard</Link>
+                </li>
+              )}
+              {!this.state.loggedIn && (
+                <li>
+                  <Link to="/login"> Login </Link>
+                </li>
+              )}
+              {this.state.loggedIn && (
+                <li>
+                  <Link to="/logout"> Logout </Link>
+                </li>
+              )}
+              </div>
+            </div>
+          </nav>
+        </div>
     );
 }
 
