172c15087cfb780e2fd82c4d22c0324c303d8283
Megan John <meganjohn.791@gmail.com>
2020-04-28 12:51:15 +0100

    Adding media query to stylesheet

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -10,18 +10,18 @@ import "./NavigationBar.css";
 
 function NavigationBar (props) {
     return (
-        <div className="sticky-nav-bar">
-          <nav className="nav-bar">
+        <section className="sticky-nav-bar">
+          <div className="nav-bar">
             <div className="left-group">
               <div className="link-div">
                 <Link to="/" className="nav-home-link"> Checkd </Link>
               </div>
               <div className="line-spacer"></div>
               <div className="link-div">
-                <Link to="/newsfeed" className="nav-general-link"> Submit News </Link>
+                <Link to="/newsfeed" className="nav-general-link"> News Feed </Link>
               </div>
               <div className="link-div">
-                <Link to="/submit-news" className="nav-general-link"> News Feed </Link>
+                <Link to="/submit-news" className="nav-general-link"> Submit News </Link>
               </div>
               <div className="link-div">
                 <Link to="/about-us" className="nav-general-link">About us</Link>
@@ -43,8 +43,8 @@ function NavigationBar (props) {
               )}
               </div>
             </div>
-          </nav>
-        </div>
+          </div>
+        </section>
     );
 }
 
