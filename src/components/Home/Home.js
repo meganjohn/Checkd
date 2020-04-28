6b1fbd7bf0122737346fa417503d698307f31b46
lawcia <lawrencianjume@gmail.com>
2020-04-28 21:31:53 +0100

    create header of homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,28 @@
 import React from "react";
 import "./Home.css";
 import photo from "./waves.png";
+import { Button } from "carbon-components-react";
+import { useHistory } from "react-router-dom";
 
-function Home() {
+function Home(props) {
+  let history = useHistory();
+
   return (
     <div className="Home">
-      <header style={{"backgroundImage": `url(${photo})`}}>
+      <header style={{ backgroundImage: `url(${photo})` }}>
         <h1>COVID-19 poses an unprecedented challenge.</h1>
-        <h2>To help facilitate the public’s access to factually correct news surrounding Coronavirus, and to reduce anxiety and negative consequences from acting on fake sources of information, a group of volunteers have created this platform.</h2>
+        <h2>
+          To help facilitate the public’s access to factually correct news
+          surrounding Coronavirus, and to reduce anxiety and negative
+          consequences from acting on fake sources of information, a group of
+          volunteers have created this platform.
+        </h2>
+        <Button
+          className="home-button"
+          onClick={() => history.push("/submit-news")}
+        >
+          Submit news
+        </Button>
       </header>
     </div>
   );
