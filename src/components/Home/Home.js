0ea687a97d1df90abc12bf0ed22410df46871de9
lawcia <lawrencianjume@gmail.com>
2020-04-28 21:23:22 +0100

    create home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Home.css";
+import photo from "./waves.png";
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="Home">
+      <header style={{"backgroundImage": `url(${photo})`}}>
+        <h1>COVID-19 poses an unprecedented challenge.</h1>
+        <h2>To help facilitate the public’s access to factually correct news surrounding Coronavirus, and to reduce anxiety and negative consequences from acting on fake sources of information, a group of volunteers have created this platform.</h2>
+      </header>
     </div>
   );
 }
