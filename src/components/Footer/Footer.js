a7cf595ec95b3e90463fad35d6d870fa786f9f78
Megan John <meganjohn.791@gmail.com>
2020-04-30 22:25:25 +0100

    Finishing styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ import {
   Link,
 } from "react-router-dom";
 import "./Footer.css";
+import emailIcon from "./email.svg";
 
 function Footer (props) {
     return (
@@ -15,8 +16,8 @@ function Footer (props) {
                 © Checkd, 2020
             </div>
             <div className="right-group">
-                <img src="./email.svg" alt="Email icon"></img>
                 <a href="mailto:checkd.news@gmail.com">
+                <img src={emailIcon} alt="Email icon"></img>
                 Contact Us
                 </a>
             </div>
